aa71b1ed0d5b9c77df8f0023fd6b306fc35f9d70
artur_fjodorov <artur.fodorov@gmail.com>
2020-04-22 12:15:33 +0300

    lint fix

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCCH4QSuBczVOT6CEmaUXqxjEG2ZZUTATk",
-  authDomain: "feix-aecf8.firebaseapp.com",
-  databaseURL: "https://feix-aecf8.firebaseio.com",
-  projectId: "feix-aecf8",
-  storageBucket: "feix-aecf8.appspot.com",
-  messagingSenderId: "1009336210667",
-  appId: "1:1009336210667:web:10966b3335449d85e59aae"
+  apiKey: 'AIzaSyCCH4QSuBczVOT6CEmaUXqxjEG2ZZUTATk',
+  authDomain: 'feix-aecf8.firebaseapp.com',
+  databaseURL: 'https://feix-aecf8.firebaseio.com',
+  projectId: 'feix-aecf8',
+  storageBucket: 'feix-aecf8.appspot.com',
+  messagingSenderId: '1009336210667',
+  appId: '1:1009336210667:web:10966b3335449d85e59aae'
 };
